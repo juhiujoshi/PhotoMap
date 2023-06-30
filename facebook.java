5c765d1058c0c4189f65c8f513c6a948a601bfe2
juhiujoshi <54783328+juhiujoshi@users.noreply.github.com>
2023-06-30 12:45:36 +0530

    Add files via upload

--- facebook.java
+++ facebook.java
@@ -1,7 +1,7 @@
 package day4;
 
-import java.time.Duration;
 
+import java.io.IOException;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,12 +9,17 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+import java.time.Duration;
 import org.testng.Assert;
 
+
 public class facebook {
-    public static void main(String[] args) {
+
+    public static void main(String[] args) throws InterruptedException, IOException {
+
         System.setProperty("webdriver.chrome.driver", "/Users/mohitjoshi/Desktop/eclipse/chromedriver");
 
+        // Disable notifications using Chrome options
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--disable-notifications");
 
@@ -34,39 +39,27 @@ public class facebook {
 
         if (loginButton.isEnabled()) {
             loginButton.click();
-
-            // Wait for the next page to load
             wait.until(ExpectedConditions.urlContains("facebook.com"));
 
             String actualUrl = driver.getCurrentUrl();
             String expectedUrl = "https://www.facebook.com/";
             Assert.assertEquals(actualUrl, expectedUrl);
 
-            // Find and click on the account menu
-            WebElement accountMenu = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[@aria-label='Account']")));
-            accountMenu.click();
 
-            // Find and click on the logout button
-            WebElement logoutButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//span[contains(text(), 'Log Out')]")));
+            // Click on Account Settings Dropdown
+            WebElement accSettings = driver.findElement(By.cssSelector(".x1i10hfl.x1qjc9v5.xjbqb8w.xjqpnuy.xa49m3k.xqeqjp1.x2hbi6w.x13fuv20.xu3j5b3.x1q0q8m5.x26u7qi.x972fbf.xcfux6l.x1qhh985.xm0m39n.x9f619.x1ypdohk.xdl72j9.x2lah0s.xe8uvvx.xdj266r.x11i5rnm.xat24cr.x1mh8g0r.x2lwn1j.xeuugli.xexx8yu.x4uap5.x18d9i69.xkhd6sd.x1n2onr6.x16tdsg8.x1hl2dhg.xggy1nq.x1ja2u2z.x1t137rt.x1o1ewxj.x3x9cwd.x1e5q0jg.x13rtm0m.x1q0g3np.x87ps6o.x1lku1pv.x1a2a7pz.xzsf02u.x1rg5ohu"));
+            accSettings.click();
+            
+            Thread.sleep(5000);
+
+            // Click on Log out button
+            WebDriverWait logoutWait = new WebDriverWait(driver, Duration.ofSeconds(8));
+            WebElement logoutButton = driver.findElement(By.xpath("//span[text()='Log out']"));
             logoutButton.click();
 
-            // Wait for the logout confirmation dialog
-            wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//div[@role='dialog']")));
 
-            // Find and click on the logout confirmation button
-            WebElement logoutConfirmButton = wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//button[contains(text(), 'Log Out')]")));
-            logoutConfirmButton.click();
-
-            // Wait for the logout to complete
-            wait.until(ExpectedConditions.urlContains("facebook.com/login"));
-
-            actualUrl = driver.getCurrentUrl();
-            expectedUrl = "https://www.facebook.com/login.php?login_attempt=1&lwv=110";
-            Assert.assertEquals(actualUrl, expectedUrl);
         } else {
             System.out.println("Login button is disabled. Please check the credentials.");
         }
-
-        driver.quit();
     }
 }
